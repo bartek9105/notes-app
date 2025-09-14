19ee93ff781e5103913c143cad1e6416caac2e4b
Bartłomiej Matoga <bartlomiej.matoga@miquido.com>
2025-09-14 20:59:33 +0200

    fix: fix build

--- src/features/notes/note-details/note-details-form/note-details-form.const.ts
+++ src/features/notes/note-details/note-details-form/note-details-form.const.ts
@@ -1,8 +1,10 @@
 import { Note } from "@/types";
 
-export const NOTE_DETAILS_FORM_DEFAULT_VALUES: Omit<Note, "created_at" | "id"> =
-  {
-    title: "",
-    description: "",
-    tags: [],
-  };
+export const NOTE_DETAILS_FORM_DEFAULT_VALUES: Omit<
+  Note,
+  "created_at" | "id" | "isArchived"
+> = {
+  title: "",
+  description: "",
+  tags: [],
+};
