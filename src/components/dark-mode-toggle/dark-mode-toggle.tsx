5fa7cf2b868c5e36ee469047ed3447cad667f4ab
Bartłomiej Matoga <bartlomiej.matoga@miquido.com>
2026-01-05 21:46:05 +0100

    feat: cleanup

--- src/components/dark-mode-toggle/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle/dark-mode-toggle.tsx
@@ -3,5 +3,6 @@ import { useTheme } from "@/providers";
 export const DarkModeToggle = () => {
   const { handleSetTheme } = useTheme();
 
+  // TODO: Add a toggle button to switch between light and dark mode
   return <div onClick={handleSetTheme}>toggler</div>;
 };
